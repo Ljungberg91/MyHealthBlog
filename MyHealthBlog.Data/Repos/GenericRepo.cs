8c0316964a2a48963121ef2795547b4dacfeeadc
Jesper Ljungberg <jeetrix1991@gmail.com>
2017-06-15 16:46:34 +0200

    Created class CategoryController, a view folder with view "Create". Added a generic repository.

---
+++ MyHealthBlog.Data/Repos/GenericRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyHealthBlog.Data.Repos
+{
+    public class GenericRepo<TEntity> where TEntity : class
+
+    {
+        public readonly MyHealthBlogContext _Context;
+
+        public GenericRepo()
+        {
+            _Context = new MyHealthBlogContext();
+        }
+        public TEntity Get(int? id)
+        {
+            return _Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _Context.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _Context.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
+
---
+++ MyHealthBlog/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyHealthBlog.Domain;
+using MyHealthBlog.Data.Repos;
+
+namespace MyHealthBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly GenericRepo<FoodCategory> _genericRepo;
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            FoodObject food = new FoodObject();
+
+            return View(food);
+        }
+        [HttpPost]
+        public IActionResult Create(FoodCategory category)
+        {
+            _genericRepo.Add(category);
+            _genericRepo._Context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
